080a036cadaa8dc7f7eb746044f5afe8e1c52a67
Natdanai Pinaves <jgogo012@gmail.com>
2024-12-25 12:53:06 -0800

    Chore: Invaild Check Condition None Type in IPv4 & IPv6

--- utils/error_status.py
+++ utils/error_status.py
@@ -1,9 +1,9 @@
 def error_status(wlan_ipv4: str, lan_ipv4: str, wlan_ipv6: str, lan_ipv6: str, 
                  wlan_dns_response_time: float, lan_dns_response_time: float) -> str:
-
+    
     errors = {
-        'IPV4': wlan_ipv4 == "" or lan_ipv4 == "",
-        'IPV6': wlan_ipv6 == "" or lan_ipv6 == "",
+        'LAN': lan_ipv4 == None or lan_ipv6 == None,
+        'WLAN': wlan_ipv4 == None or wlan_ipv6 == None,
         'DNS': wlan_dns_response_time == 0 or lan_dns_response_time == 0
     }
     
