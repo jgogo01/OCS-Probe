6b1903570db3bdc206601b9b7e311efaf89fec65
Natdanai Pinaves <jgogo012@gmail.com>
2025-01-04 11:25:13 -0800

    Fix: Remove Link-Local, ULA in IPv6

--- utils/ip_by_interface.py
+++ utils/ip_by_interface.py
@@ -5,8 +5,18 @@ def get_ip_addresses(interface_name):
         addrs = netifaces.ifaddresses(interface_name)
         
         ipv4 = addrs[netifaces.AF_INET][0]['addr'] if netifaces.AF_INET in addrs else None
-        ipv6 = addrs[netifaces.AF_INET6][0]['addr'] if netifaces.AF_INET6 in addrs else None
-            
+        ipv6 = None
+        if netifaces.AF_INET6 in addrs:
+            for addr in addrs[netifaces.AF_INET6]:
+                if 'addr' in addr:
+                    current_ipv6 = addr['addr']
+                    # Link-local (fe80::) and ULA (fc00:: หรือ fd00::)
+                    if not (current_ipv6.startswith('fe80:') or 
+                           current_ipv6.startswith('fc00:') or 
+                           current_ipv6.startswith('fd00:')):
+                        ipv6 = current_ipv6
+                        break
+                    
         return {
             "IPv4": ipv4,
             "IPv6": ipv6
